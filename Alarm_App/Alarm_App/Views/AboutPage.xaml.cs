eed284f3fdc6632f17c00b8bc1acdc373925f84b
pablosky21 <plsalgado@gmail.com>
2017-10-02 23:07:15 -0300

    Agregar archivos de proyecto.

---
+++ Alarm_App/Alarm_App/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Alarm_App.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
